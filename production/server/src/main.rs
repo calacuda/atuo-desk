99f1cd22e96f26ee565deccdc1103df44e32ea87
yogurt <eowest@gmail.com>
2022-05-24 22:26:19 -0400

    added a logging message

--- production/server/src/main.rs
+++ production/server/src/main.rs
@@ -114,6 +114,8 @@ fn load_layout(spath: &str, args: &str) -> u8 {
         }
     };
 
+    println!("[LOG] loading layout {} from", file_path);
+
     return if file_path.ends_with(".yml") || file_path.ends_with(".yaml") {
         load_from_yaml(layout_file, spath, &file_path)
     } else {
